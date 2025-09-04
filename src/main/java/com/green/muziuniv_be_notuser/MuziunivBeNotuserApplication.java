c678f2e6316c1aecafbfa1ae6597b6414001d00e
1ethon0822 <1ethyjj9240@gmail.com>
2025-09-04 11:08:20 +0900

    not user

---
+++ src/main/java/com/green/muziuniv_be_notuser/MuziunivBeNotuserApplication.java
@@ -0,0 +1,13 @@
+package com.green.muziuniv_be_notuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuziunivBeNotuserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuziunivBeNotuserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/muziuniv_be_notuser/MuziunivBeNotuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.muziuniv_be_notuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuziunivBeNotuserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
