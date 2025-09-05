7329c18564c727fe86ce31d9418a89279a9071d5
yongwa1214 <yongwa1214@gmail.com>
2025-09-05 16:29:57 +0900

    entity 생성 완료

--- src/main/java/com/green/muziuniv_be_notuser/entity/schedule/Schedule.java
+++ src/main/java/com/green/muziuniv_be_notuser/entity/schedule/Schedule.java
@@ -1,4 +1,37 @@
 package com.green.muziuniv_be_notuser.entity.schedule;
 
-public class Schedule {
+
+import com.green.muziuniv_be_notuser.entity.CreatedAt;
+import com.green.muziuniv_be_notuser.entity.semester.Semester;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Schedule extends CreatedAt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int scheduleId;
+
+    @ManyToOne
+    @JoinColumn(name = "semester_id", nullable = false)
+    private Semester semesterId;
+
+    @Column(nullable = false, length = 20)
+    private String schedule_type;
+
+    @Embedded // 연결
+    private TimeSetting timeSetting;
+
+    @Column(nullable = false)
+    private String description;
+
+
 }
