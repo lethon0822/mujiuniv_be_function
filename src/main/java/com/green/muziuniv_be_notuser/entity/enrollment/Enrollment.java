d0666af2d505959480db0f10e0423b291eb8f49e
ongsoony8382 <ongsoony8382@gmail.com>
2025-09-05 17:25:24 +0900

    enrollment 엔티티 작성

--- src/main/java/com/green/muziuniv_be_notuser/entity/enrollment/Enrollment.java
+++ src/main/java/com/green/muziuniv_be_notuser/entity/enrollment/Enrollment.java
@@ -1,4 +1,41 @@
 package com.green.muziuniv_be_notuser.entity.enrollment;
 
-public class Enrollment {
+import com.green.muziuniv_be_notuser.entity.CreatedAt;
+import com.green.muziuniv_be_notuser.entity.UserId;
+import com.green.muziuniv_be_notuser.entity.course.Course;
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+        name = "enrollment",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                     columnNames = {
+                          "course_id", "user_id"
+                     }
+                )
+        }
+
+)
+public class Enrollment extends CreatedAt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long enrollmentId;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @Embedded
+    private UserId userId; //학생 아이디
+
+    @Column(length = 255)
+    private String review;
+
+    @Column(name = "ev_score")
+    private int evScore;
+
+    @Column(length = 50)
+    private String status;
+
 }
