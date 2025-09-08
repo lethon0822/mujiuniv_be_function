7da79906a31d5a6987c9cd8500628250672d27b3
ongsoony8382 <ongsoony8382@gmail.com>
2025-09-08 16:59:09 +0900

    attendance entity 생성

--- src/main/java/com/green/muziuniv_be_notuser/entity/attendance/Attendance.java
+++ src/main/java/com/green/muziuniv_be_notuser/entity/attendance/Attendance.java
@@ -1,4 +1,25 @@
 package com.green.muziuniv_be_notuser.entity.attendance;
 
-public class Attendance {
+import com.green.muziuniv_be_notuser.entity.UpdatedAt;
+import com.green.muziuniv_be_notuser.entity.enrollment.Enrollment;
+import jakarta.persistence.*;
+
+
+
+@Entity
+public class Attendance extends UpdatedAt {
+
+    @EmbeddedId
+    private AttendanceIds attendanceIds;
+
+    @MapsId("enrollmentId")
+    @ManyToOne
+    @JoinColumn(name = "enrollment_id", nullable = false)
+    private Enrollment enrollment;
+
+    @Column(length = 50, nullable = false)
+    private String status = "결석";
+
+    @Column(length = 100)
+    private String note;
 }
---
+++ src/main/java/com/green/muziuniv_be_notuser/entity/attendance/AttendanceIds.java
@@ -0,0 +1,18 @@
+package com.green.muziuniv_be_notuser.entity.attendance;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Embeddable
+@EqualsAndHashCode
+public class AttendanceIds implements Serializable {
+    @Column(name = "attend_date", nullable = false)
+    private LocalDate attendDate;
+    @Column(name = "enrollment_id", nullable = false )
+    private int enrollmentId;
+}
