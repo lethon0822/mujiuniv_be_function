750c8f8743775bda3a11a3c0f1d0922c018ad61f
ongsoony8382 <ongsoony8382@gmail.com>
2025-09-09 16:04:51 +0900

    ofen-feign user로 수정

--- src/main/java/com/green/muziuniv_be_notuser/openfeign/user/model/UserClient.java
+++ src/main/java/com/green/muziuniv_be_notuser/openfeign/user/model/UserClient.java
@@ -3,6 +3,6 @@ package com.green.muziuniv_be_notuser.openfeign.user.model;
 import com.green.muziuniv_be_notuser.common.util.FeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "${constants.open-feign.function}", configuration = FeignConfiguration.class)
+@FeignClient(name = "${constants.open-feign.user}", configuration = FeignConfiguration.class)
 public interface UserClient {
 }
