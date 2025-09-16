0bb9322ed27a58325d7618823bc22c256cb36c7a
taewon1918 <taewon1918@gmail.com>
2025-09-16 17:30:46 +0900

    Oragne 작업내용
    AttendanceService => postman 실시 (DB저장 성공)
    ScoreController => RequestMapping 주소에 api 제거

--- src/main/java/com/green/muziuniv_be_notuser/app/professor/score/ScoreController.java
+++ src/main/java/com/green/muziuniv_be_notuser/app/professor/score/ScoreController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/professor/course/grade")
+@RequestMapping("/professor/course/grade")
 @RequiredArgsConstructor
 public class ScoreController {
 
