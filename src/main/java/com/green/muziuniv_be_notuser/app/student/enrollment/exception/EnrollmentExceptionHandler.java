2129c4ee63b8600ce230ef8c54fa235923b375af
ongsoony8382 <ongsoony8382@gmail.com>
2025-09-16 09:46:05 +0900

    수강신청 오류 핸들러 수정

--- src/main/java/com/green/muziuniv_be_notuser/app/student/enrollment/exception/EnrollmentExceptionHandler.java
+++ src/main/java/com/green/muziuniv_be_notuser/app/student/enrollment/exception/EnrollmentExceptionHandler.java
@@ -1,11 +1,12 @@
 package com.green.muziuniv_be_notuser.app.student.enrollment.exception;
 
+import com.green.muziuniv_be_notuser.app.student.enrollment.EnrollmentController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(assignableTypes = {EnrollmentController.class})
 public class EnrollmentExceptionHandler {
     //EnrollmentException 터지면 여기서 잡힘.
     @ExceptionHandler(EnrollmentException.class)
@@ -15,11 +16,4 @@ public class EnrollmentExceptionHandler {
                 .body(e.getMessage());
     }
 
-    //서버에러 처리
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e){
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("수강신청 실패! 서버 오류가 발생했습니다.");
-    }
 }
