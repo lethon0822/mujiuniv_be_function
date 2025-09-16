cdc8e782ad7b79cf42e2015e1b758979013ada94
ongsoony8382 <ongsoony8382@gmail.com>
2025-09-16 10:57:28 +0900

    수강신청 구현 완료

--- src/main/java/com/green/muziuniv_be_notuser/app/shared/course/CourseRepository.java
+++ src/main/java/com/green/muziuniv_be_notuser/app/shared/course/CourseRepository.java
@@ -1,10 +1,12 @@
 package com.green.muziuniv_be_notuser.app.shared.course;
 
 import com.green.muziuniv_be_notuser.entity.course.Course;
-import com.green.muziuniv_be_notuser.app.student.enrollment.model.EnrollmentRes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course,Long> {
-    EnrollmentRes findCourseByCourseId(Long courseId);
+   Optional<Course> findCourseByCourseId(Long courseId);
 }
