94c061bb52d72b4f7d2ea1f4ed78975cf7d4436a
ongsoony8382 <ongsoony8382@gmail.com>
2025-09-19 16:33:01 +0900

    금학기 성적 조회 완료

--- src/main/java/com/green/muziuniv_be_notuser/app/student/grade/GradeMapper.java
+++ src/main/java/com/green/muziuniv_be_notuser/app/student/grade/GradeMapper.java
@@ -2,6 +2,7 @@ package com.green.muziuniv_be_notuser.app.student.grade;
 
 import com.green.muziuniv_be_notuser.app.student.grade.model.GetAllPermanentGradeReq;
 import com.green.muziuniv_be_notuser.app.student.grade.model.GetAllPermanentGradeRes;
+import com.green.muziuniv_be_notuser.app.student.grade.model.GetMyCurrentGradeRes;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface GradeMapper {
     // 영구 성적 조회
     List<GetAllPermanentGradeRes> getAllPermanentGrade(Long userId, GetAllPermanentGradeReq req);
+    // 금학기 성적 조회
+    List<GetMyCurrentGradeRes> getMyCurrentGrade(Long userId, int semesterId);
+
 }
