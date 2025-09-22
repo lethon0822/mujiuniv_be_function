6f220a15b4a01444f398e9f4bd50716735e341e4
ongsoony8382 <ongsoony8382@gmail.com>
2025-09-22 14:30:24 +0900

    강의 조회 교양학부 처리 완료

--- src/main/java/com/green/muziuniv_be_notuser/app/shared/courseEvaluation/CourseEvaluationController.java
+++ src/main/java/com/green/muziuniv_be_notuser/app/shared/courseEvaluation/CourseEvaluationController.java
@@ -3,6 +3,7 @@ package com.green.muziuniv_be_notuser.app.shared.courseEvaluation;
 import com.green.muziuniv_be_notuser.app.shared.courseEvaluation.model.CourseEvaluationReq;
 import com.green.muziuniv_be_notuser.configuration.model.SignedUser;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("/evaluation")
+@RequestMapping("/course/evaluation")
 @RequiredArgsConstructor
 public class CourseEvaluationController {
     private final CourseEvaluationService courseEvaluationService;
@@ -20,6 +22,7 @@ public class CourseEvaluationController {
     @PutMapping
     public ResponseEntity<?> courseEvaluation(@AuthenticationPrincipal SignedUser signedUser, @RequestBody CourseEvaluationReq req) {
         Long userId = signedUser.signedUserId;
+        log.info(">>> 강의 평가 요청 들어옴");
         return courseEvaluationService.courseEvaluation(userId, req);
     }
 }
