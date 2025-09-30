2577eb51dea6c1e0832f83f67c2cb39b06a1b110
ongsoony8382 <ongsoony8382@gmail.com>
2025-09-30 11:02:22 +0900

    강의평가 여부에 따른 성적 null 처리

--- src/main/java/com/green/muziuniv_be_notuser/app/student/grade/model/GetMyCurrentGradeRes.java
+++ src/main/java/com/green/muziuniv_be_notuser/app/student/grade/model/GetMyCurrentGradeRes.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 @Setter
 public class GetMyCurrentGradeRes {
     private Long enrollmentId; // 금학기 강의평가 req용으로 쓰기 위함
+
     private Long courseId;
     private String type;
     private Long userId;
@@ -15,6 +16,7 @@ public class GetMyCurrentGradeRes {
     private String courseCode;
     private String title;
     private int credit;
+
     private String rank;
     private Integer midScore;
     private Integer finScore;
@@ -22,6 +24,7 @@ public class GetMyCurrentGradeRes {
     private Integer otherScore;
     private Double point;
 
+    // 강의 평가를 했다면 진짜 성적을 알려주고, 강의 평가를 안 했다면 가려서 처리
     private Integer evScore;
 
 }
