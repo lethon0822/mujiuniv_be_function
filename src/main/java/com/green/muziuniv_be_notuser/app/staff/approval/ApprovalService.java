7ecaed16bf3cccad20a93b48df876ee4e1cfda0a
yongwa1214 <yongwa1214@gmail.com>
2025-10-10 18:49:21 +0900

    [fix] Approval course 필터바 학과 추가

--- src/main/java/com/green/muziuniv_be_notuser/app/staff/approval/ApprovalService.java
+++ src/main/java/com/green/muziuniv_be_notuser/app/staff/approval/ApprovalService.java
@@ -7,6 +7,8 @@ import com.green.muziuniv_be_notuser.app.staff.approval.model.ApprovalCoursePatc
 import com.green.muziuniv_be_notuser.app.staff.approval.model.ApprovalCoursePatchRes;
 import com.green.muziuniv_be_notuser.app.staff.approval.model.CoursePendingRes;
 import com.green.muziuniv_be_notuser.entity.course.Course;
+import com.green.muziuniv_be_notuser.openfeign.department.DepartmentClient;
+import com.green.muziuniv_be_notuser.openfeign.department.model.DepartmentHeadNameRes;
 import com.green.muziuniv_be_notuser.openfeign.user.UserClient;
 import com.green.muziuniv_be_notuser.openfeign.user.model.UserInfoDto;
 import com.green.muziuniv_be_notuser.configuration.model.ResultResponse;
@@ -26,6 +28,7 @@ public class ApprovalService {
     private final ApprovalMapper approvalMapper;
     private final UserClient userClient;
     private final CourseRepository courseRepository;
+    private final DepartmentClient departmentClient;
 
     /**
      * 신청 목록 조회
@@ -103,6 +106,23 @@ public class ApprovalService {
 
     // 처리중 강의만 DTO 변환해서 반환
     public List<CoursePendingRes> getPendingCourses(CoursePendingReq req) {
+
+        if (req.getDeptId() != null) {
+            // 1. 유저 서비스에 호출해서 해당 학과의 교수 리스트 가져오기
+            ResultResponse<List<DepartmentHeadNameRes>> deptProfessors = departmentClient.findDeptHeadList(req.getDeptId());
+            // 2. 교수 userId 리스트만 추출
+            List<Long> professorIds = deptProfessors.getResult().stream()
+                    .map(d -> d.getUserId())
+                    .collect(Collectors.toList());
+            // 학과 아이디로 교수 리스트를 가져왔는데 그 교수들이 개설한 강의가 없는 경우 -> 매퍼에서 if 조건문이 빠지면서 조회 결과가
+            // 이상해지므로 빈 리스트 반환
+            if (professorIds.isEmpty()) {
+                return Collections.emptyList();
+            }
+            // 3. 요청 객체에 교수 리스트 세팅 (courseMapper에서 in 조건으로 사용)
+            req.setProfessorIds(professorIds);
+        }
+
         List<CoursePendingRes> courseList = approvalMapper.findCoursesByStatus(req);
         if(courseList.isEmpty()){
             return null;
--- src/main/java/com/green/muziuniv_be_notuser/app/staff/approval/model/CoursePendingReq.java
+++ src/main/java/com/green/muziuniv_be_notuser/app/staff/approval/model/CoursePendingReq.java
@@ -3,10 +3,15 @@ package com.green.muziuniv_be_notuser.app.staff.approval.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class CoursePendingReq {
     private Long sid;
     private String type;
     private String keyword;
+
+    private Long deptId;
+    private List<Long> professorIds;
 }
