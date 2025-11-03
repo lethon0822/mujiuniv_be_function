153a20758c6ecc57a5529422551f467491f3fc5b
ongsoony8382 <ongsoony8382@gmail.com>
2025-11-03 21:39:04 +0900

    gpa 계산 로직 매퍼에서 서비스로 이사

--- src/main/java/com/green/muziuniv_be_notuser/app/student/gpa/GpaMapper.java
+++ src/main/java/com/green/muziuniv_be_notuser/app/student/gpa/GpaMapper.java
@@ -1,6 +1,7 @@
 package com.green.muziuniv_be_notuser.app.student.gpa;
 
 import com.green.muziuniv_be_notuser.app.student.gpa.model.GpaRes;
+import com.green.muziuniv_be_notuser.app.student.gpa.model.GpaRowDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Mapper
 public interface GpaMapper {
-    List<GpaRes> getMyGpa(@Param("userId") Long userId, @Param("semesterId") Long semesterId);
+    List<GpaRowDTO> findAllGradesForGpa (@Param("userId") Long userId, @Param("semesterId") Long semesterId);
 }
